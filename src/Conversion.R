2c498a5da62c3c7632ad6fabad06967ab56e02dd
MiguelAngelLiera <miguelliera@ciencias.unam.mx>
2023-08-21 11:34:02 -0600

    o

--- src/Conversion.R
+++ src/Conversion.R
@@ -1,4 +1,4 @@
-datos <- read.csv("~/Documentos/articulos/analisis/Datos históricos GFINBURO.csv", header = TRUE, sep = ",")
-cierre <- datos[["Cierre"]]
+datos <- read.csv("D:/MALIERA/Documents/[00] Stock_Exchange/Stock_Exchange_NN_PP/src/COMI Historical Data.csv", header = TRUE, sep = ",")
+cierre2 <- datos[["Cierre"]]
 
-write.csv(cierre, file = "cierre.csv", row.names = FALSE)
+write.csv(cierre2, file = "cierre2.csv", row.names = FALSE)
